b6bbedd1a89015218860e2dde416e4afe2be35a0
Yashashree Bedmutha <101212250+yashhashhrrreee@users.noreply.github.com>
2022-12-08 00:28:07 +0530

    Update Merging Two Dictionaries.py

--- Merging Two Dictionaries.py
+++ Merging Two Dictionaries.py
@@ -1,28 +1,52 @@
 import pyinputplus as pyip
+from collections import defaultdict
 
 def Merge(dict1, dict2): 
    for key, value in dict2.items():
       if key in dict1:
          dict1[key] = [value , dict1[key]]
       else:
-         dict1[key]=value
-   return(dict1)   
+         dict1[key]=value  
+   return(dict1)
 
-n1= pyip.inputInt(prompt='Enter The no of inputs In Dictionary 1:',min=0)
-d1 = {}
-for i in range(n1):
-    keys = pyip.inputNum("Enter the Key:")
-    values = pyip.inputStr("Enter the Value:")
-    d1[keys] = values
-print(d1)
+print('------------------------------------------------------------------')
+n1= pyip.inputInt(prompt='Enter Size of Dictionary 1 : ',min=0)
+stu=[]
+l=[]
+for i in range(0,n1):
+    stu.append(pyip.inputNum("Enter the Key: "))
+    stu.append(pyip.inputStr("Enter the Value: "))
+    l.append(stu)
+    stu = []
+d1 = defaultdict(list)
+for k, v in l:
+    d1[k].append(v)
+d1= dict((k1, tuple(v1)) for k1,v1 in d1.items())
+print('------------------------------------------------------------------')
+print("Dictionary 1:")
+for k, v in d1.items():
+    print(k, v)
 
-n2= pyip.inputInt(prompt='Enter The no of inputs In Dictionary 2:',min=0)
-d2 = {}
-for i in range(n2):
-    keys = pyip.inputNum("Enter the Key :")
-    values = pyip.inputStr("Enter the Value:")
-    d2[keys] = values
-print(d2)
+print('------------------------------------------------------------------')
+n2= pyip.inputInt(prompt='Enter Size of Dictionary 2 : ',min=0)
+st=[]
+l1=[]
+for i in range(0,n2):
+    st.append(pyip.inputNum("Enter the Key: "))
+    st.append(pyip.inputStr("Enter the Value: "))
+    l1.append(st)
+    st = []
+d2 = defaultdict(list)
+for k1, v1 in l1:
+    d2[k1].append(v1)
+d2= dict((k1, tuple(v1)) for k1,v1 in d2.items())
+print('------------------------------------------------------------------')
+print("Dictionary 2:")
+for k, v in d2.items():
+    print(k, v)
 
 d3 = (Merge(d1, d2)) 
-print("\nDictionary after merging ::>",d3)
+print('------------------------------------------------------------------\n')
+print("\nDictionary after merging ::>")
+print(d3)
+print('------------------------------------------------------------------')
